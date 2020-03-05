ebc80429cd689a285f32cad99a85213494a3f0a5
cjchopin <49544999+cjchopin@users.noreply.github.com>
2020-03-06 00:33:53 +0100

    Delete 3_Coinflip_migration.js

--- 3_Coinflip_migration.js
+++
@@ -1,7 +0,0 @@
-const Coinflip = artifacts.require("Coinflip");
-
-module.exports = function(deployer) {
-  deployer.deploy(Coinflip);
-};
-// let instance = await Coinflip.deployed();
-// instance.inputMoney({from: accounts[0], value: 40000000000000000000})
